3aad3a523eec12a43b684e2b266e8643569db8f8
im24a-bieris <bieris@bzz.ch>
2026-08-19 21:53:15 +0200

    allow network dev origin

--- VideoWebsite/next.config.ts
+++ VideoWebsite/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost"],
+  allowedDevOrigins: ["127.0.0.1", "localhost", "172.24.208.1"],
 };
 
 export default nextConfig;
